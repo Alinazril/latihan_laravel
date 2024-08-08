1c9ecce3ddabb718fdb775bac851021795d9e3c8
Alinazril <alialinazril@gmail.com>
2024-08-08 10:56:53 +0700

    sanctum

--- routes/api.php
+++ routes/api.php
@@ -11,12 +11,12 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::middleware((['auth:sanctum']))->group(function () {
+// Route::middleware((['auth:sanctum']))->group(function () {
     Route::post('logout', [AuthController::class, 'logout']);
     Route::resource('kategori', KategoriController::class);
     Route::resource('genre', GenreController::class);
     Route::resource('aktor', AktorController::class);
-});
+// });
 
 Route::post('login', [AuthController::class, 'login']);
 Route::post('register', [AuthController::class, 'register']);
